136105389e47b4d3b7c70d24384d03626d248dd5
Gabriele Mainetti <mg02.gabriele@gmail.com>
2023-01-11 17:34:11 +0100

    header menu

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -4,13 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
     
-    <h1>HELLO WORLD</h1>
+    <h1 class="text-center">HELLO WORLD</h1>
     <nav>
-        <ul>
+        <ul class="d-flex justify-content-around">
             <li>
                 <a href="/">
                     HOME
@@ -25,6 +26,7 @@
             </li>
         </ul>
     </nav>
+
 
 </body>
 </html>
